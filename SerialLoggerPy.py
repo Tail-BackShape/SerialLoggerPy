4e167955996ecb7d88044702e6dbd1900d568da4
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-02-16 23:16:00 +0900

    fix SerialLoggerPy.py
    Added the ability to save received serial communications to a file.

--- SerialLoggerPy.py
+++ SerialLoggerPy.py
@@ -1,16 +1,23 @@
 import serial
 
 def main():
+
+    filename = input("Enter filename: ")
+    f = open("./log/" + filename + ".txt","w+")
+
     try:
         COMPort = input("Enter COM Port: ")
         ser = serial.Serial(COMPort, 9600)
 
         while True:
-            data = ser.readline()
-            print(data.decode('utf-8').strip())
+            data = (ser.readline()).decode('utf-8')
+            print(data.strip())
+            f.write(data.strip() + "\n")
 
+    # Close serial port on keyboard interrupt
     except KeyboardInterrupt:
         ser.close()
+        f.close()
 
 if __name__ == "__main__":
     main()
