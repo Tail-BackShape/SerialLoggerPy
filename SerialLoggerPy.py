8f15190245433e5de94c1c6c94d547c117b3767e
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-02-18 16:59:28 +0900

    fix SerialLoggerPy.py
    Added the ability to output a list of COM ports on the command line.

--- SerialLoggerPy.py
+++ SerialLoggerPy.py
@@ -1,4 +1,6 @@
 import serial
+import serial.tools.list_ports
+
 from datetime import datetime
 
 def main():
@@ -6,12 +8,18 @@ def main():
     filename = input("Enter filename: ")
     f = open("./log/" + filename + ".txt","w+")
 
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        print(p)
+
+    COMPort = input("Enter COM Port: ")
+
     try:
-        COMPort = input("Enter COM Port: ")
+
         ser = serial.Serial(COMPort, 9600)
 
         while True:
-            now = datetime.now().strftime("%Y-%m-%d, %H:%M:%S, ")
+            now = datetime.now().strftime("%Y-%m-%d, %H:%M:%S,")
             data = (ser.readline()).decode('utf-8')
             print(now, data.strip())
             f.write(now)
