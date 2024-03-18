651415d7417175617f38f76cb9ab20635ffc4f05
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-03-18 23:50:42 +0900

    add Tester.ino
    for check SerialLoggerPy to see if it is working

---
+++ Tester/Tester.ino
@@ -0,0 +1,13 @@
+// SerialLoggerPyの動作テスト用プログラム
+// シリアル割り込みで受信したデータをそのまま送信する
+// 割り込み確認のためにカウンターをインクリメントして送信する
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
